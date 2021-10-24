635163a4f5057571bcac161efc96754929b79f24
Matt <matthewastro@gmail.com>
2021-10-24 02:16:08 -0400

    Bug Tracker 1st Commit 🚀

---
+++ src/main/java/com/codingdojo/loginregistration/BugTrackerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.loginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/loginregistration/BugTrackerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
